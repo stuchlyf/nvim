cad42b50fe5dc49cff95257ace63f0be36d6d72f
Florian Stuchly <stuchlyf@stuchlyf.dev>
2024-03-16 02:37:36 +0100

    init

---
+++ init.lua
@@ -0,0 +1,23 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- Check if 'pwsh' is executable and set the shell accordingly
+if vim.fn.executable("pwsh") == 1 then
+  vim.o.shell = "pwsh"
+else
+  vim.o.shell = "powershell"
+end
+
+-- Setting shell command flags
+vim.o.shellcmdflag =
+  "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
+
+-- Setting shell redirection
+vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+
+-- Setting shell pipe
+vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
+
+-- Setting shell quote options
+vim.o.shellquote = ""
+vim.o.shellxquote = ""
