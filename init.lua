2c0b5dff1612703ffea49adc228e69a2b9a99f22
Florian Stuchly <stuchlyf@stuchlyf.dev>
2024-03-16 15:50:15 +0100

    feat(shell): Add conditional configuration of shell

--- init.lua
+++ init.lua
@@ -3,23 +3,29 @@ require("config.lazy")
 
 vim.api.nvim_exec("language en_US", true)
 
--- Check if 'pwsh' is executable and set the shell accordingly
-if vim.fn.executable("pwsh") == 1 then
-  vim.o.shell = "pwsh"
-else
-  vim.o.shell = "powershell"
+local function windows_shell()
+  -- Setting shell command flags
+  vim.o.shellcmdflag =
+    "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
+
+  -- Setting shell redirection
+  vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+
+  -- Setting shell pipe
+  vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
+
+  -- Setting shell quote options
+  vim.o.shellquote = ""
+  vim.o.shellxquote = ""
 end
 
--- Setting shell command flags
-vim.o.shellcmdflag =
-  "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
-
--- Setting shell redirection
-vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
-
--- Setting shell pipe
-vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
-
--- Setting shell quote options
-vim.o.shellquote = ""
-vim.o.shellxquote = ""
+-- Check if 'pwsh' is executable and set the shell accordingly
+if vim.fn.executable("fish") == 1 then
+  vim.o.shell = "fish"
+elseif vim.fn.executable("pwsh") == 1 then
+  vim.o.shell = "pwsh"
+  windows_shell()
+else
+  vim.o.shell = "powershell"
+  windows_shell()
+end
