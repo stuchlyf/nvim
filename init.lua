94db80b2ca8bc0ad521dac7a69c6f1136f7358de
Florian Stuchly <stuchlyf@stuchlyf.dev>
2024-03-16 03:15:18 +0100

    fix(language): Set language explicitly to english.

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.api.nvim_exec("language en_US", true)
+
 -- Check if 'pwsh' is executable and set the shell accordingly
 if vim.fn.executable("pwsh") == 1 then
   vim.o.shell = "pwsh"
